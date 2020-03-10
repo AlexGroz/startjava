a8281cd23d8ab681b4bce5b6eafaf1162d44a130
Alexey <groznovaa@gmail.comls>
2020-03-11 00:33:38 +0500

    modified lesson_5

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -2,3 +2,15 @@
 
 CREATE DATABASE Jaegers;
 
+CREATE TABLE Jaegers (
+	id SERIAL PRIMARY KEY,
+	modelName TEXT,
+	mark INTEGER,
+	height INTEGER,
+	weight INTEGER,
+	status TEXT,
+	origin TEXT,
+	launch DATE,
+	kaijuKill INTEGER);
+
+
--- src/com/startjava/lesson_5/querise.sql
+++ src/com/startjava/lesson_5/querise.sql
@@ -1,24 +1,24 @@
 -- querise
 
-jaegers=# SELECT * FROM jaegers;
+SELECT * FROM jaegers;
 
-jaegers=# SELECT * FROM jaegers WHERE status = 'Active';
+SELECT * FROM jaegers WHERE status = 'Active';
 
-jaegers=# SELECT * FROM jaegers WHERE mark = 1 or mark = 6;
+SELECT * FROM jaegers WHERE mark = 1 or mark = 6;
 
-jaegers=# SELECT * FROM jaegers ORDER BY mark DESC;
+SELECT * FROM jaegers ORDER BY mark DESC;
 
-jaegers=# SELECT * FROM jaegers ORDER BY mark DESC;
+SELECT * FROM jaegers ORDER BY mark DESC;
 
-jaegers=# SELECT * FROM Jaegers WHERE kaijukill = (SELECT MIN(kaijukill) FROM Jaegers);
+SELECT * FROM Jaegers WHERE kaijukill = (SELECT MIN(kaijukill) FROM Jaegers);
 
-jaegers=# SELECT * FROM Jaegers WHERE kaijukill = (SELECT MAX(kaijukill) FROM Jaegers);
+SELECT * FROM Jaegers WHERE kaijukill = (SELECT MAX(kaijukill) FROM Jaegers);
 
-jaegers=# SELECT AVG(weight) FROM jaegers;
+SELECT AVG(weight) FROM jaegers;
 
-jaegers=# UPDATE jaegers SET kaijukill = kaijukill +1  WHERE status = 'Active';
+UPDATE jaegers SET kaijukill = kaijukill + 1  WHERE status = 'Active';
 
-jaegers=# DELETE FROM jaegers WHERE status = 'Destroyed';
+DELETE FROM jaegers WHERE status = 'Destroyed';
 
 
 
